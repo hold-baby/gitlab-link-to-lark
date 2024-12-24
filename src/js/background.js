cc3b8274b721bcbccc31fff6107d028d0c3d4861
hold-baby <hold_baby@qq.com>
2024-12-24 14:48:46 +0800

    feat: 新增 prefetch_list 数据源支持

--- src/js/background.js
+++ src/js/background.js
@@ -29,12 +29,20 @@ async function main() {
   });
 }
 
-async function getLarkProjectInfo({ tid, app }) {
-  const [t, id] = tid.split("-");
-  const type = t === "m" ? "story" : "issue";
-  let url = `${LARK_DOMAIN_HOST}/${app}/${type}/detail/${id}`;
-  const res = await fetch(url);
-  const text = await res.text();
+const extractScriptContent = (text) => {
+  // 使用正则表达式匹配 script 标签内的内容
+  const scriptRegex = /<script[^>]*>([\s\S]*?)<\/script>/;
+  const match = text.match(scriptRegex);
+
+  if (match && match[1]) {
+    // 返回标签内的内容
+    return match[1].trim();
+  }
+
+  return ""; // 如果没有找到匹配，返回空字符串
+};
+
+const getLarkProjectInfoByDetail = (text) => {
   const reg = /<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi;
   const scripts = text.match(reg);
   let content = "";
@@ -52,7 +60,6 @@ async function getLarkProjectInfo({ tid, app }) {
   if (!content)
     return {
       error: true,
-      tid,
       data: null,
     };
   content = content.replace(/\n/g, "");
@@ -73,9 +80,81 @@ async function getLarkProjectInfo({ tid, app }) {
   data = obj[key].data;
   return {
     error: false,
-    tid,
     data,
   };
+};
+
+const getLarkProjectInfoByPrefetchList = (text) => {
+  const reg = /<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi;
+  const scripts = text.match(reg);
+  let content = "";
+  for (let i = 0; i < scripts.length; i++) {
+    const text = scripts[i];
+    const isContain =
+      text.includes('...{"APIDemandFetchWorkItem":') &&
+      text.includes(`window.prefetch_list`);
+
+    if (isContain) {
+      content = text;
+      break;
+    }
+  }
+  if (!content)
+    return {
+      error: true,
+      data: null,
+    };
+  content = content.replace(/\n/g, "");
+  content = content.replace(new RegExp("\\\\x3C", "g"), "<");
+  content = content.replace(new RegExp("\x3C", "g"), "<");
+  content = content.replace(new RegExp("\\x3C", "g"), "<");
+  const noScriptContent = extractScriptContent(content);
+  if (noScriptContent) {
+    content = noScriptContent;
+  }
+  content = content.replace("window.prefetch_list = {", "");
+  content = content.replace("...window.prefetch_list,", "");
+  content = content.replace("...{", "{");
+  content = content.replace("} };", "}");
+  content = content.replace(/\\'/g, "'");
+  const obj = JSON.parse(content);
+  const biz_data = obj.APIDemandFetchWorkItem.data.data.biz_data;
+  const target = biz_data.find((item) => item.key === "workitem");
+  if (!target) {
+    return {
+      error: true,
+      data: null,
+    };
+  }
+  return {
+    error: false,
+    data: {
+      data: target.value,
+    },
+  };
+};
+
+async function getLarkProjectInfo({ tid, app }) {
+  const [t, id] = tid.split("-");
+  const type = t === "m" ? "story" : "issue";
+  let url = `${LARK_DOMAIN_HOST}/${app}/${type}/detail/${id}`;
+  const res = await fetch(url);
+  const text = await res.text();
+  let info = { tid };
+  const detailInfo = getLarkProjectInfoByDetail(text);
+  if (detailInfo.data) {
+    info = {
+      ...info,
+      ...detailInfo.data,
+    };
+  } else {
+    const prefetchListInfo = getLarkProjectInfoByPrefetchList(text);
+    info = {
+      ...info,
+      ...prefetchListInfo.data,
+    };
+  }
+  return info;
 }
 
 chrome.runtime.onInstalled.addListener((details) => {
